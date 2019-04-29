d6c0361fa59913d68bf8aa3d7a6691906b5a19bd
Igor Rozani <igorrozani@gmail.com>
2019-04-29 17:38:45 -0300

    Genre api

---
+++ backend/MovieDatabase/MovieDatabase.API/Controllers/GenreController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MovieDatabase.API.TMDB;
+
+namespace MovieDatabase.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<GenreResponse> GetGenres()
+        {
+            var genres = new GenreAPI().GetGenresAsync();
+
+            return Ok(genres.Result);
+
+        }
+    }
+}
