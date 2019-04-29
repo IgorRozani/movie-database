75dd4a1ed024c741c38d7f2899fec8a1ccc82bee
Igor Rozani <igorrozani@gmail.com>
2019-04-29 16:06:55 -0300

    Movie Controller

---
+++ backend/MovieDatabase/MovieDatabase.API/Controllers/MovieController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieDatabase.API.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public IActionResult GetMovie(int id)
+        {
+            return null;
+        }
+
+            
+    }
+}
