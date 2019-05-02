f140075d17a316eadec86b8d3d4626d617ebbd8a
Igor Rozani <igorrozani@gmail.com>
2019-05-01 21:02:35 -0300

    Genre type and list css

--- frontend/src/app/movie/model/movie-details.ts
+++ frontend/src/app/movie/model/movie-details.ts
@@ -4,4 +4,5 @@ export class MovieDetails {
     overview: string
     ReleaseDate: Date
     posterPath: string
+    genres:string
 }
--- frontend/src/app/movie/model/movie-list-item.ts
+++ frontend/src/app/movie/model/movie-list-item.ts
@@ -6,9 +6,9 @@ export class MovieListItem {
     releaseDate: Date
     posterPath: string
     backdropPath: string
-    genres: Genre[]
+    genres: string
 
-    constructor(id: number, title: string, releaseDate: Date, posterPath: string, backdropPath: string, genres: Genre[]) {
+    constructor(id: number, title: string, releaseDate: Date, posterPath: string, backdropPath: string, genres: string) {
         this.id = id
         this.title = title
         this.releaseDate = releaseDate
