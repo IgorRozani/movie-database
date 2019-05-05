5c4b93847371a7728d68a334282ae3daae13c339
Igor Rozani <igorrozani@gmail.com>
2019-05-05 20:21:09 -0300

    Refactoring RestClient

--- backend/MovieDatabase.RestClient/Services/RestClient.cs
+++ backend/MovieDatabase.RestClient/Services/RestClient.cs
@@ -1,6 +1,7 @@
 ﻿using Flurl;
 using Flurl.Http;
 using MovieDatabase.RestClient.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -8,17 +9,28 @@ namespace MovieDatabase.RestClient.Services
 {
     public class RestClient : IRestClient
     {
-        public async Task<T> GetJsonAsync<T>(string url, string path, Dictionary<string, string> parameters)
+        public Task<T> GetJsonAsync<T>(string url, string path, Dictionary<string, string> parameters)
         {
-            var restUrl = new Url(url).AppendPathSegment(path);
+            var serviceUrl = new Url(url).AppendPathSegment(path);
 
             if (parameters != null)
             {
                 foreach (var parameter in parameters)
-                    restUrl.SetQueryParam(parameter.Key, parameter.Value);
+                    serviceUrl.SetQueryParam(parameter.Key, parameter.Value);
             }
 
-            return await restUrl.GetJsonAsync<T>();
+            try
+            {
+                return serviceUrl.GetJsonAsync<T>();
+            }
+            catch(FlurlHttpException ex)
+            {
+                return Task.FromResult<T>(default);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult<T>(default);
+            }
         }
     }
 }
