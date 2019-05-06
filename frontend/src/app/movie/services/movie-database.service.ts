57062ec72661ea114e5db5f4f1cfc52bb5830c16
Igor Rozani <igorrozani@gmail.com>
2019-05-05 23:11:03 -0300

    Fix movie-database.service

--- frontend/src/app/movie/services/movie-database.service.ts
+++ frontend/src/app/movie/services/movie-database.service.ts
@@ -9,16 +9,19 @@ import { MovieListItem } from '../models/movie-list-item';
 })
 export class MovieDatabaseService {
 
-  private baseApi = 'http://localhost:4200/api/';
+  private baseApi = 'http://localhost:4200/api/movies';
 
   constructor(private http: HttpClient) { }
 
   getMovieDetail(id: number): Observable<MovieDetails> {
-    return this.http.get<MovieDetails>(this.baseApi + 'movie/' + id);
+    return this.http.get<MovieDetails>(this.baseApi + '/' + id);
   }
 
   getMovies(page: number = 1, quantityPage: number = 40, movieName: string = null): Observable<MovieListItem[]> {
-    console.log(`${this.baseApi}movie?page=${page}&quantityPage=${quantityPage}&movieName=${movieName}`)
-    return this.http.get<MovieListItem[]>(`${this.baseApi}movie?page=${page}&quantityPage=${quantityPage}&movieName=${movieName}`);
+    let url = `${this.baseApi}?page=${page}&quantityPage=${quantityPage}`
+    if (movieName != null)
+      url += `&movieName=${movieName}`
+
+    return this.http.get<MovieListItem[]>(url);
   }
 }
